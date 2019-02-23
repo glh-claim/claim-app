e9eeb194b72697ddcb132cd87a88dd04dbbd85d7
Ralfi Salhon <rifat.salhon@tufts.edu>
2019-02-23 14:21:33 +0800

    Upload evidence

--- src/EvidenceScreen.js
+++ src/EvidenceScreen.js
@@ -7,6 +7,7 @@ import {
 	SafeAreaView,
 } from 'react-native';
 import { StackActions, NavigationActions } from 'react-navigation';
+import { Dropdown } from 'react-native-material-dropdown';
 
 export default class EvidenceScreen extends Component
 {
@@ -18,11 +19,32 @@ export default class EvidenceScreen extends Component
 	constructor()
 	{
 		super();
+		this.state = {
+			data: [
+				{ value: 'Receipt' },
+				{ value: 'Ticket' },
+				{ value: 'Identification' }
+			]
+		};
+	}
+
+	renderItems() {
+		return(
+			<View style = {{flex: 1, paddingHorizontal: 50, backgroundColor: 'lightgray', marginVertical: 25}}>
+				<Dropdown
+					baseColor={"gray"}
+					label='Type of Document'
+					data={this.state.data}/>
+					<TouchableOpacity style = {{height: 50, backgroundColor: 'gray', marginBottom: 15, borderRadius: 100, justifyContent: 'center', alignItems: 'center'}} activeOpacity={0.5} onPress={() => Alert.alert("Upload button")}>
+						<Text style = {{color: 'white', fontFamily: 'Avenir Next', fontWeight: '500'}}>Upload</Text>
+					</TouchableOpacity>
+			</View>
+		);
 	}
 
 	renderButton() {
 		return(
-			<View style = {{flex: 1, justifyContent: 'flex-end'}}>
+			<View style = {{justifyContent: 'flex-end'}}>
 				<TouchableOpacity style = {{height: 50, backgroundColor: 'lightgray', marginBottom: 15, marginHorizontal: 100, borderRadius: 100, justifyContent: 'center', alignItems: 'center'}} activeOpacity={0.5} onPress={() => this.props.navigation.navigate("evidence")}>
 					<Text>Continue</Text>
 				</TouchableOpacity>
@@ -45,6 +67,7 @@ export default class EvidenceScreen extends Component
 						Upload relevant documents or photos to support your claim
 					</Text>
 				</View>
+				{this.renderItems()}
 				{this.renderButton()}
 			</SafeAreaView>
 		);
