2237bbf852b2ae06e1612f344c96a0cce7487b22
Ralfi Salhon <rifat.salhon@tufts.edu>
2019-02-24 13:00:29 +0800

    Give reviews added

--- src/ConfirmationScreen.js
+++ src/ConfirmationScreen.js
@@ -5,9 +5,12 @@ import {
     TouchableOpacity,
     Alert,
 	SafeAreaView,
+	Dimensions,
 } from 'react-native';
 import { StackActions, NavigationActions } from 'react-navigation';
 import { Dropdown } from 'react-native-material-dropdown';
+import { Icon } from "native-base";
+const { width: windowWidth, height: windowHeight } = Dimensions.get('window');
 
 export default class EvidenceScreen extends Component
 {
@@ -19,47 +22,12 @@ export default class EvidenceScreen extends Component
 	constructor()
 	{
 		super();
-		this.state = {
-			data: [
-				{ value: 'Receipt' },
-				{ value: 'Ticket' },
-				{ value: 'Identification' }
-			],
-			type: "",
-		};
-	}
-
-	typeChanged(text) {
-		this.setState({
-			type: text,
-		});
-	}
-
-	renderItems() {
-		return(
-			<View style = {{flex: 1}}>
-				<View style = {{marginHorizontal: 40, marginBottom: 10}}>
-					<Dropdown
-					baseColor={"gray"}
-					label='Type of Document'
-					data={this.state.data}
-					onChangeText={(text) => this.typeChanged(text)}/>
-				</View>
-
-				<TouchableOpacity style = {{height: 50, backgroundColor: (this.state.type && this.state.type.length > 1 ? 'white' : 'lightgray'), marginBottom: 15, marginHorizontal: 100, borderRadius: 100, justifyContent: 'center', alignItems: 'center'}} activeOpacity={0.5} onPress={(this.state.type && this.state.type.length > 1 ? () => Alert.alert("Upload Pressed") : () => Alert.alert("Please pick a document type"))}>
-					<Text style = {{fontFamily: 'Avenir Next', color: '#00355f', textAlign: 'center', fontSize: 16}}>Upload</Text>
-				</TouchableOpacity>
-			</View>
-		);
 	}
 
 	renderButton() {
 		return(
 			<View style = {{justifyContent: 'flex-end'}}>
-				<TouchableOpacity style = {{height: 50, backgroundColor: 'white', marginBottom: 15, marginHorizontal: 100, borderRadius: 100, justifyContent: 'center', alignItems: 'center'}} activeOpacity={0.5} onPress={() => this.props.navigation.navigate("summary")}>
-					<Text style = {{fontFamily: 'Avenir Next', color: '#00355f', textAlign: 'center', fontSize: 16}}>Prepare Claim</Text>
-				</TouchableOpacity>
-				<TouchableOpacity style = {{height: 50, backgroundColor: 'white', marginBottom: 15, marginHorizontal: 100, borderRadius: 100, justifyContent: 'center', alignItems: 'center'}} activeOpacity={0.5} onPress={() => this.props.navigation.goBack()}>
+				<TouchableOpacity style = {{height: 50, backgroundColor: 'white', marginBottom: 15, marginHorizontal: 100, borderRadius: 100, justifyContent: 'center', alignItems: 'center'}} activeOpacity={0.5} onPress={() => this.props.navigation.navigate("pick")}>
 					<Text style = {{fontFamily: 'Avenir Next', color: '#00355f', textAlign: 'center', fontSize: 16}}>Go Back</Text>
 				</TouchableOpacity>
 			</View>
@@ -70,15 +38,23 @@ export default class EvidenceScreen extends Component
 	{
 		return(
 			<SafeAreaView style = {styles.container}>
-				<View style = {{justifyContent: 'center', alignItems: 'center', marginTop: 25}}>
-					<Text style = {{fontFamily: 'Avenir Next', color: '#00355f', fontSize: 24, fontWeight: '500'}}>
-						Upload Evidence
-					</Text>
-					<Text style = {{fontFamily: 'Avenir Next', color: '#33447a', fontSize: 14, fontWeight: '500', textAlign: 'center', paddingHorizontal: 50}}>
-						Upload relevant documents or photos to support your claim
-					</Text>
+				<View style = {{flex: 1}}>
+					<View style = {{flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginTop: 25}}>
+						<Icon name="checkbox-marked-circle" type="MaterialCommunityIcons" style={{ fontSize: 80, color: "#00355f", marginRight: 15 }} />
+						<Text style = {{color: "#00355f", fontFamily: 'Avenir Next', fontSize: 18, fontWeight: '500'}}>Your case have been{"\n"}sent to the company. </Text>
+					</View>
+					<View style = {{margin: 20, backgroundColor: 'white', borderRadius: 20, padding: 10}}>
+						<Text style = {{color: "#00355f", fontFamily: 'Avenir Next', fontSize: 16, fontWeight: '400'}}>
+						Claim No. 903924{"\n"}
+						If you do not receive response form the company within 2 weeks, come back to CLAIMate to publicise your case!{"\n"}{"\n"}
+						Authenticity of cases is crucially important in supporting the operation of our App. {"\n"}{"\n"}Would you mind spending several minutes to review the cases that others have filed?
+						</Text>
+
+						<TouchableOpacity style = {{height: 50, backgroundColor: '#00355f', marginTop: 15, marginHorizontal: 75, borderRadius: 100, justifyContent: 'center', alignItems: 'center'}} activeOpacity={0.5} onPress={() => this.props.navigation.navigate("review")}>
+							<Text style = {{fontFamily: 'Avenir Next', color: 'white', textAlign: 'center', fontSize: 16}}>Give Reviews</Text>
+						</TouchableOpacity>
+					</View>
 				</View>
-				{this.renderItems()}
 				{this.renderButton()}
 			</SafeAreaView>
 		);
